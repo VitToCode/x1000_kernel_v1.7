d33389f32438946c7fa3562f559e2500b9f04b53
jbbi <jbbi@ingenic.cn>
2012-07-26 10:36:09 +0800

    add README and tools/ln.sh

---
+++ drivers/nand/tools/ln.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+    SRC=$1
+    DST=`pwd`
+    echo "find $DST/../ -name \"*.[hc]\""
+    for i in `find $DST/../ -name "*.[hc]"`; do
+	DFILE=$i;
+	if [ ! -d $DFILE ]; then
+	    echo "find $SRC -name $(basename $DFILE) | grep -v 'thirdpart' | grep -v 'testmodule'"
+	    let t=0;
+	    for j in `find $SRC -name $(basename $DFILE) | grep -v "thirdpart"  | grep -v "testmodule"`; do
+		let t=t+1
+		if [ $t -ge 2 ]; then
+		    echo "error"
+		    exit
+		fi
+		rm $DFILE
+		echo "ln -s "$j $DFILE
+		ln -s $SRC/"$SFILE" $DFILE
+	    done
+	fi
+    done
+else
+    echo "./ln.sh <source dir>"
+fi
