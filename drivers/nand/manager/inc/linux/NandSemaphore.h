66c0728e6cf4f9db67ddff8b383c232de650fd30
dsqiu <dsqiu@ingenic.cn>
2012-08-04 13:56:37 +0800

    add debug mutex

--- drivers/nand/manager/inc/linux/NandSemaphore.h
+++ drivers/nand/manager/inc/linux/NandSemaphore.h
@@ -4,6 +4,8 @@
 #include <linux/errno.h>
 #include <linux/semaphore.h>
 #include <linux/mutex.h>
+
+
 typedef struct semaphore NandSemaphore;
 //val 1 is unLock val 0 is Lock
 static inline void InitSemaphore(NandSemaphore*sem,int val){
@@ -29,6 +31,8 @@ static inline void Semaphore_signal(NandSemaphore* sem){
 	up(sem);
 }
 
+#define DEBUG_NDMUTEX
+
 typedef struct mutex NandMutex;
 static inline void InitNandMutex(NandMutex *mutex){
 	mutex_init(mutex);
@@ -36,11 +40,15 @@ static inline void InitNandMutex(NandMutex *mutex){
 static inline void DeinitNandMutex(NandMutex *mutex){
 	mutex_destroy(mutex);
 }
-
+#ifdef DEBUG_NDMUTEX
+#define NandMutex_Lock(x) do{mutex_lock(x);printk("%s %d lock\n",__FILE__,__LINE__);}while(0)
+#define NandMutex_Unlock(x) do{printk("%s %d unlock\n",__FILE__,__LINE__);mutex_unlock(x);}while(0)
+#else
 static inline void NandMutex_Lock(NandMutex *mutex){
 	mutex_lock(mutex);
 }
 static inline void NandMutex_Unlock(NandMutex* mutex){
 	mutex_unlock(mutex);
 }
+#endif
 #endif /* _NANDSEMPHORE_H_ */
