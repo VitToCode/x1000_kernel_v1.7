acc4e3fc7da793ab270c14a80b1370c0c1d1d5fd
ljia <ljia@ingenic.cn>
2012-09-06 11:52:54 +0800

    Modified for 4780: Bring up the priority of CPU on both AHB0 & AHB2
    Change-Id: Ife98c5f046d2dfee75dc06b819f9c779b0b2a6f3

--- arch/mips/xburst/soc-4780/common/setup.c
+++ arch/mips/xburst/soc-4780/common/setup.c
@@ -1,6 +1,6 @@
 /*
  * JZSOC Clock and Power Manager
- * 
+ *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
@@ -18,10 +18,27 @@
 #include <soc/base.h>
 #include <soc/extal.h>
 
+/*
+ * Bring up the priority of CPU on both AHB0 & AHB2
+ */
+void __init setup_priority(unsigned int base, unsigned int target, unsigned int value)
+{
+        if(base != HARB0_IOBASE)
+                if(base != HARB2_IOBASE)
+                        printk("%s: Invalid value.\n", __FUNCTION__);
+
+        if(value > 3 || target > 20)
+                printk("%s: Invalid value.\n", __FUNCTION__);
+
+        printk("%s: BUS--0x%x, TARGET--0x%x, VALUE--0x%x\n", __FUNCTION__, base, target, value);
+        outl((inl(base) | (value << target)), base);
+        printk("%s: VALUE after setup--0x%x\n", __FUNCTION__, inl(base));
+}
+
 void __init cpm_reset(void)
 {
 	unsigned long lcr = cpm_inl(CPM_LCR);
-	
+
 	cpm_outl(lcr | CPM_LCR_PD_MASK,CPM_LCR);
 	while((cpm_inl(CPM_LCR) & (0x7<<24)) != (0x7<<24));
 
@@ -34,6 +51,9 @@ void __init cpm_reset(void)
 int __init setup_init(void)
 {
 	cpm_reset();
+        // CPU on AHB0 & AHB2
+        setup_priority(HARB0_IOBASE, 6, 3);
+        setup_priority(HARB2_IOBASE, 10, 3);
 
 	return 0;
 }
