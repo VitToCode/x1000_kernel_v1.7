2f70b5a9317815defae334fca60ba573f54dc2bd
ztyan <ztyan@ingenic.cn>
2012-09-27 11:19:51 +0800

    add vcore low down and cpu clock down for power sleep
    Change-Id: Ib79f434855440cf7c43f690eb18f3d5d46d9c126

--- arch/mips/xburst/soc-4780/common/pm.c
+++ arch/mips/xburst/soc-4780/common/pm.c
@@ -31,12 +31,25 @@
 #include <linux/delay.h>
 #include <asm/cacheops.h>
 #include <asm/rjzcache.h>
+#include <linux/syscore_ops.h>
+#include <linux/regulator/consumer.h>
 
 #include <soc/cache.h>
 #include <soc/base.h>
 #include <soc/cpm.h>
 
 void (*__reset_dll)(void);
+
+struct jz47xx_pm {
+	int volt;
+	unsigned int cpccr;
+	struct regulator *vcore;
+	struct syscore_ops vcore_pm_ops;
+	struct platform_suspend_ops pm_ops;
+};
+
+static struct jz47xx_pm jz47xx_pm;
+
 
 void inline reset_dll(void)
 {
@@ -140,17 +153,48 @@ static int jz4780_pm_enter(suspend_state_t state)
 	return 0;
 }
 
-static struct platform_suspend_ops jz4780_pm_ops = {
-	.valid		= suspend_valid_only_mem,
-	.enter		= jz4780_pm_enter,
-};
+int vcore_suspend(void)
+{
+	if(!jz47xx_pm.vcore)
+		jz47xx_pm.vcore = regulator_get(NULL,"vcore");
+
+	jz47xx_pm.cpccr = cpm_inl(CPM_CPCCR);
+	cpm_outl((jz47xx_pm.cpccr & ~0xff) | 0x73,CPM_CPCCR);
+
+	if(jz47xx_pm.vcore) {
+		jz47xx_pm.volt = regulator_get_voltage(jz47xx_pm.vcore);
+		regulator_set_voltage(jz47xx_pm.vcore, 1000000, 1000000);
+	}
+
+	return 0;
+}
+
+void vcore_resume(void)
+{
+	if(!jz47xx_pm.vcore)
+		jz47xx_pm.vcore = regulator_get(NULL,"vcore");
+
+	if(jz47xx_pm.vcore && jz47xx_pm.volt) {
+		regulator_set_voltage(jz47xx_pm.vcore, jz47xx_pm.volt, jz47xx_pm.volt);
+		jz47xx_pm.volt = 0;
+	}
+
+	cpm_outl(jz47xx_pm.cpccr,CPM_CPCCR);
+}
 
 /*
  * Initialize power interface
  */
+
 int __init jz4780_pm_init(void)
 {
-	suspend_set_ops(&jz4780_pm_ops);
+	jz47xx_pm.vcore_pm_ops.suspend = vcore_suspend;
+	jz47xx_pm.vcore_pm_ops.resume = vcore_resume;
+	register_syscore_ops(&jz47xx_pm.vcore_pm_ops);
+
+	jz47xx_pm.pm_ops.valid = suspend_valid_only_mem,
+	jz47xx_pm.pm_ops.enter = jz4780_pm_enter,
+	suspend_set_ops(&jz47xx_pm.pm_ops);
 	return 0;
 }
 
