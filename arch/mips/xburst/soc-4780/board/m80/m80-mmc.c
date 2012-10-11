950837cf1bbedac313d651d7f4c7dc99f2234f7c
SunWenZhong <wzsun@ingenic.cn>
2012-10-11 18:57:22 +0800

    mmc: add a mark for hard set MSC clock to top
    Change-Id: I2dac0031d567fb2293358b153acd6a0abe0ca7c4

--- arch/mips/xburst/soc-4780/board/m80/m80-mmc.c
+++ arch/mips/xburst/soc-4780/board/m80/m80-mmc.c
@@ -84,6 +84,7 @@ static struct card_gpio m80_tf_gpio = {
 struct jzmmc_platform_data m80_tf_pdata = {
 	.removal  			= REMOVABLE,
 	.sdio_clk			= 0,
+	.top_speed			= 1,
 	.ocr_avail			= MMC_VDD_32_33 | MMC_VDD_33_34,
 	.capacity  			= MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
 	.recovery_info			= NULL,
