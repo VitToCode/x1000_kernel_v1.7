bc5231082323bac915a515db81439bc369d986d2
ZhouKui[Eternal] <kzhou@ingenic.cn>
2012-10-19 11:54:21 +0800

    modify kernel compile error for m80 and m80701
    Change-Id: I3636e0bc02af358d165821614c34cfccbe8276d4

--- arch/mips/xburst/soc-4780/board/m80/m80-misc.c
+++ arch/mips/xburst/soc-4780/board/m80/m80-misc.c
@@ -17,6 +17,7 @@
 #include <linux/power/jz4780-battery.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_gpio.h>
+#include <linux/jz_dwc.h>
 
 #include <mach/platform.h>
 #include <mach/jzsnd.h>
