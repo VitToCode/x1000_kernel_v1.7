8a6667872f178ac06d7f30631aa7ff362fc87def
jliu <jliu@ingenic.cn>
2012-10-24 10:34:38 +0800

    add hdmi-80
    Change-Id: I24d78e84db850ffe5d58ef911dd8ad022a47acf8

---
+++ arch/mips/xburst/soc-4780/board/hdmi-80/hdmi-80-regulator.c
@@ -0,0 +1,159 @@
+/*
+ * [board]-pmu.c - This file defines PMU board information.
+ *
+ * Copyright (C) 2012 Ingenic Semiconductor Co., Ltd.
+ * Author: Large Dipper <ykli@ingenic.com>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/platform_device.h>
+#include <linux/regulator/machine.h>
+#include <linux/mfd/pmu-common.h>
+#include <linux/i2c.h>
+#include <gpio.h>
+
+/**
+ * Core voltage Regulator.
+ * Couldn't be modified.
+ * Voltage was inited at bootloader.
+ */
+CORE_REGULATOR_DEF(
+	hdmi_80,	1000000,	1350000);
+
+/**
+ * I/O Regulator.
+ * It's the parent regulator of most of devices regulator on board.
+ * Voltage was inited at bootloader.
+ */
+IO_REGULATOR_DEF(
+	hdmi_80_vccio,
+	"Vcc-IO",	3300000,	1);
+
+/**
+ * USB VBUS Regulators.
+ * Switch of USB VBUS. It may be a actual or virtual regulator.
+ */
+VBUS_REGULATOR_DEF(
+	hdmi_80,	"OTG-Vbus");
+
+/**
+ * Exclusive Regulators.
+ * They are only used by one device each other.
+ */
+EXCLUSIVE_REGULATOR_DEF(
+	hdmi_80_vwifi,
+	"Wi-Fi",
+	"vwifi",	NULL,		3300000);
+
+EXCLUSIVE_REGULATOR_DEF(
+	hdmi_80_vtsc,
+	"Touch Screen",
+	"vtsc",		NULL,		3300000);
+
+EXCLUSIVE_REGULATOR_DEF(
+	hdmi_80_vgsensor,
+	"G-sensor",
+	"vgsensor",	NULL,		3300000);
+
+/**
+ * Fixed voltage Regulators.
+ * GPIO silulator regulators. Everyone is an independent device.
+ */
+FIXED_REGULATOR_DEF(
+	hdmi_80_vcc5,
+	"Vcc-5V",	5000000,
+	GPIO_PA(17),	HIGH_ENABLE,	0,
+	NULL,		"vhdmi",	"jz-hdmi");
+
+FIXED_REGULATOR_DEF(
+	hdmi_80_vbus,
+	"OTG-Vbus",	5000000,
+	GPIO_PE(10),	HIGH_ENABLE,	0,
+	"Vcc-5V",	"vdrvvbus",	NULL);
+
+FIXED_REGULATOR_DEF(
+	hdmi_80_vcim,
+	"Camera",	2800000,
+	GPIO_PB(27),	HIGH_ENABLE,	0,
+	NULL,		"vcim",		"jz-cim");
+
+FIXED_REGULATOR_DEF(
+	hdmi_80_vlcd,
+	"LCD",		3300000,
+	GPIO_PB(23),	HIGH_ENABLE,	0,
+	NULL,		"vlcd",		NULL);
+
+static struct platform_device *fixed_regulator_devices[] __initdata = {
+	&hdmi_80_vcc5_regulator_device,
+	&hdmi_80_vbus_regulator_device,
+	&hdmi_80_vcim_regulator_device,
+	&hdmi_80_vlcd_regulator_device,
+};
+
+/*
+ * Regulators definitions used in PMU.
+ *
+ * If +5V is supplied by PMU, please define "VBUS" here with init_data NULL,
+ * otherwise it should be supplied by a exclusive DC-DC, and you should define
+ * it as a fixed regulator.
+ */
+static struct regulator_info hdmi_80_pmu_regulators[] = {
+	{"OUT1", &hdmi_80_vcore_init_data},
+	{"OUT2", &hdmi_80_vccio_init_data},
+	{"OUT6", &hdmi_80_vwifi_init_data},
+	{"OUT7", &hdmi_80_vtsc_init_data},
+	{"OUT8", &hdmi_80_vgsensor_init_data},
+	{"VBUS", &hdmi_80_vbus_init_data},
+};
+
+static struct charger_board_info charger_board_info = {
+	.gpio	= GPIO_PB(2),
+	.enable_level	= LOW_ENABLE,
+};
+
+static struct pmu_platform_data hdmi_80_pmu_pdata = {
+	.gpio = GPIO_PA(28),
+	.num_regulators = ARRAY_SIZE(hdmi_80_pmu_regulators),
+	.regulators = hdmi_80_pmu_regulators,
+	.charger_board_info = &charger_board_info,
+};
+
+#define PMU_I2C_BUSNUM 0
+
+struct i2c_board_info hdmi_80_pmu_board_info = {
+	I2C_BOARD_INFO("act8600", 0x5a),
+	.platform_data = &hdmi_80_pmu_pdata,
+};
+
+static int __init hdmi_80_pmu_dev_init(void)
+{
+	struct i2c_adapter *adap;
+	struct i2c_client *client;
+	int busnum = PMU_I2C_BUSNUM;
+	int i;
+
+	adap = i2c_get_adapter(busnum);
+	if (!adap) {
+		pr_err("failed to get adapter i2c%d\n", busnum);
+		return -1;
+	}
+
+	client = i2c_new_device(adap, &hdmi_80_pmu_board_info);
+	if (!client) {
+		pr_err("failed to register pmu to i2c%d\n", busnum);
+		return -1;
+	}
+
+	i2c_put_adapter(adap);
+
+	for (i = 0; i < ARRAY_SIZE(fixed_regulator_devices); i++)
+		fixed_regulator_devices[i]->id = i;
+
+	return platform_add_devices(fixed_regulator_devices,
+				    ARRAY_SIZE(fixed_regulator_devices));
+}
+
+subsys_initcall_sync(hdmi_80_pmu_dev_init);
