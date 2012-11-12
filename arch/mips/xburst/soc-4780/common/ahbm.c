93559f0b3a83cf4835a3a1d06c2105b295c52e70
ztyan <ztyan@ingenic.cn>
2012-11-12 11:22:46 +0800

    update ahb monitor proc interface for scripts
    Change-Id: Ie2d88a20a8934c186a60e9cfe2f93e0cb1b15aa8

--- arch/mips/xburst/soc-4780/common/ahbm.c
+++ arch/mips/xburst/soc-4780/common/ahbm.c
@@ -20,40 +20,11 @@
 
 static int msecs = 1;
 
-#define DDR_MC		0x130100E4
-#define DDR_RESULT_1	0x130100D4
-#define DDR_RESULT_2	0x130100D8
-#define DDR_RESULT_3	0x130100DC
-#define DDR_RESULT_4	0x130100E0
-
 static int ahbm_read_proc(char *page, char **start, off_t off,
 		int count, int *eof, void *data)
 {
 	int len = 0;
 	cpm_clear_bit(11,CPM_CLKGR1);
-#define PRINT(ARGS...) len += sprintf (page+len, ##ARGS)
-#define PRINT_ARRAY(M) \
-	do{					\
-		PRINT("0x04 = %d\n",ahbm_read(M,0x04));	\
-		PRINT("0x08 = %d\n",ahbm_read(M,0x08));	\
-		PRINT("0x10 = %d\n",ahbm_read(M,0x10));	\
-		PRINT("0x14 = %d\n",ahbm_read(M,0x14));	\
-		PRINT("0x18 = %d\n",ahbm_read(M,0x18));	\
-		PRINT("0x1c = %d\n",ahbm_read(M,0x1c));	\
-		PRINT("0x20 = %d\n",ahbm_read(M,0x20));	\
-		PRINT("0x24 = %d\n",ahbm_read(M,0x24));	\
-		PRINT("0x28 = %d\n",ahbm_read(M,0x28));	\
-	}while(0);
-
-#define PRINT_ARRAY_GPU(M) \
-	do{					\
-		PRINT_ARRAY(M); \
-		PRINT("0x2c = %d\n",ahbm_read(M,0x2c));	\
-		PRINT("0x30 = %d\n",ahbm_read(M,0x30));	\
-		PRINT("0x34 = %d\n",ahbm_read(M,0x34));	\
-		PRINT("0x38 = %d\n",ahbm_read(M,0x38));	\
-		PRINT("0x3c = %d\n",ahbm_read(M,0x3c));	\
-	}while(0);
 
 	outl(0x0,DDR_MC);
 	outl(0x0,DDR_RESULT_1);
@@ -79,23 +50,17 @@ static int ahbm_read_proc(char *page, char **start, off_t off,
 	PRINT("msecs = %d\n",msecs);
 
 	outl(0x0,DDR_MC);
-	PRINT("DDR:\n");
-	PRINT("RESULT_1:%d\n",inl(DDR_RESULT_1));
-	PRINT("RESULT_2:%d\n",inl(DDR_RESULT_2));
-	PRINT("RESULT_3:%d\n",inl(DDR_RESULT_3));
-	PRINT("RESULT_4:%d\n",inl(DDR_RESULT_4));
+	PRINT("DDR(%x):%x\n",DDR_RESULT_1,inl(DDR_RESULT_1));
+	PRINT("DDR(%x):%x\n",DDR_RESULT_2,inl(DDR_RESULT_2));
+	PRINT("DDR(%x):%x\n",DDR_RESULT_3,inl(DDR_RESULT_3));
+	PRINT("DDR(%x):%x\n",DDR_RESULT_4,inl(DDR_RESULT_4));
 
-	PRINT("CIM\n");
-	PRINT_ARRAY(CIM);
-	PRINT("AHB0\n");
-	PRINT_ARRAY(AHB0);
-	PRINT("AHB2\n");
-	PRINT_ARRAY(AHB2);
-
-	PRINT("GPU\n");
-	PRINT_ARRAY_GPU(GPU);
-	PRINT("LCD\n");
-	PRINT_ARRAY_GPU(LCD);
+	PRINT_ARRAY(0,CIM);
+	PRINT_ARRAY(1,AHB0);
+	PRINT_ARRAY2(2,GPU);
+	PRINT_ARRAY2(3,LCD);
+	PRINT_ARRAY2(4,XXX);
+	PRINT_ARRAY(5,AHB2);
 
 	cpm_set_bit(11,CPM_CLKGR1);
 	return len;
--- arch/mips/xburst/soc-4780/include/soc/ahbm.h
+++ arch/mips/xburst/soc-4780/include/soc/ahbm.h
@@ -2,10 +2,17 @@
 #ifndef __INCLUDE_AHBM_
 #define __INCLUDE_AHBM_
 
+#define DDR_MC		0x130100E4
+#define DDR_RESULT_1	0x130100D4
+#define DDR_RESULT_2	0x130100D8
+#define DDR_RESULT_3	0x130100DC
+#define DDR_RESULT_4	0x130100E0
+
 #define AHBM_CIM_IOB		0x130f0000
 #define AHBM_AHB0_IOB	 	0x130f0100
 #define AHBM_GPU_IOB 		0x130f0200
 #define AHBM_LCD_IOB 		0x130f0300
+#define AHBM_XXX_IOB 		0x13230000
 #define AHBM_AHB2_IOB		0x134c0000
 
 #define ahbm_restart(M)			\
@@ -20,6 +27,32 @@ do{ 								\
 }while(0)
 
 #define ahbm_read(M,off) inl((AHBM_##M##_IOB)+(off))
+
+#define PRINT(ARGS...) len += sprintf (page+len, ##ARGS)
+
+#define PRINT_ARRAY(n,M) \
+	do{					\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x04),ahbm_read(M,0x04));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x08),ahbm_read(M,0x08));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x10),ahbm_read(M,0x10));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x14),ahbm_read(M,0x14));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x18),ahbm_read(M,0x18));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x1c),ahbm_read(M,0x1c));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x20),ahbm_read(M,0x20));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x24),ahbm_read(M,0x24));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x28),ahbm_read(M,0x28));	\
+	}while(0);
+
+#define PRINT_ARRAY2(n,M) \
+	do{					\
+		PRINT_ARRAY(n,M); \
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x2c),ahbm_read(M,0x2c));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x30),ahbm_read(M,0x30));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x34),ahbm_read(M,0x34));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x38),ahbm_read(M,0x38));	\
+		PRINT("CH%d(%x):%x\n",n,(AHBM_##M##_IOB+0x3c),ahbm_read(M,0x3c));	\
+	}while(0);
+
 
 #endif
 
