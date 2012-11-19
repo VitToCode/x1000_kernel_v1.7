2c778b80f8f4ffe6e11169b5c3f7d48871750271
JiaDongWei(Ryder) <dwjia@ingenic.cn>
2012-11-19 16:43:14 +0800

    add support for tf card as storage deviece
    Change-Id: I22133a30fd7a8f9d9e24b1fa48f5beab68f904e8

--- arch/mips/xburst/soc-4780/board/ji8070a/ji8070a-mmc.c
+++ arch/mips/xburst/soc-4780/board/ji8070a/ji8070a-mmc.c
@@ -19,7 +19,7 @@
 static struct wifi_data			iw8101_data;
 
 int iw8101_wlan_init(void);
-
+#ifndef CONFIG_NAND_JZ4780
 #ifdef CONFIG_MMC0_JZ4780
 struct mmc_partition_info ji8070a_inand_partition_info[] = {
 	[0] = {"mbr",           0,       512, 0}, 	//0 - 512KB
@@ -57,6 +57,65 @@ struct jzmmc_platform_data ji8070a_inand_pdata = {
 	.private_init			= NULL,
 };
 #endif
+#ifdef CONFIG_MMC2_JZ4780
+/*
+ * WARING:
+ * If a GPIO is not used or undefined, it must be set -1,
+ * or PA0 will be request.
+ */
+static struct card_gpio ji8070a_tf_gpio = {
+	.cd				= {GPIO_PF(20),		LOW_ENABLE},
+	.wp				= {-1,			-1},
+	.pwr				= {-1,			-1},
+};
+
+struct jzmmc_platform_data ji8070a_tf_pdata = {
+	.removal  			= REMOVABLE,
+	.sdio_clk			= 0,
+	.ocr_avail			= MMC_VDD_32_33 | MMC_VDD_33_34,
+	.capacity  			= MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
+	.max_freq			= CONFIG_MMC2_MAX_FREQ,
+	.recovery_info			= NULL,
+	.gpio				= &ji8070a_tf_gpio,
+#ifdef CONFIG_MMC0_PIO_MODE
+	.pio_mode			= 1,
+#else
+	.pio_mode			= 0,
+#endif
+	.private_init			= NULL,
+};
+#endif
+#else
+#ifdef CONFIG_MMC0_JZ4780
+/*
+ * WARING:
+ * If a GPIO is not used or undefined, it must be set -1,
+ * or PA0 will be request.
+ */
+static struct card_gpio ji8070a_tf_gpio = {
+	.cd				= {GPIO_PF(20),		LOW_ENABLE},
+	.wp				= {-1,			-1},
+	.pwr				= {-1,			-1},
+};
+
+struct jzmmc_platform_data ji8070a_tf_pdata = {
+	.removal  			= REMOVABLE,
+	.sdio_clk			= 0,
+	.ocr_avail			= MMC_VDD_32_33 | MMC_VDD_33_34,
+	.capacity  			= MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
+	.max_freq			= CONFIG_MMC0_MAX_FREQ,
+	.recovery_info			= NULL,
+	.gpio				= &ji8070a_tf_gpio,
+#ifdef CONFIG_MMC0_PIO_MODE
+	.pio_mode			= 1,
+#else
+	.pio_mode			= 0,
+#endif
+	.private_init			= NULL,
+};
+#endif
+#endif
+
 #ifdef CONFIG_MMC1_JZ4780
 struct jzmmc_platform_data ji8070a_sdio_pdata = {
 	.removal  			= MANUAL,
@@ -74,44 +133,46 @@ struct jzmmc_platform_data ji8070a_sdio_pdata = {
 	.private_init			= iw8101_wlan_init,
 };
 #endif
-#if defined(CONFIG_MMC2_JZ4780) || defined(CONFIG_MMC0_JZ4780)
-/*
- * WARING:
- * If a GPIO is not used or undefined, it must be set -1,
- * or PA0 will be request.
- */
-static struct card_gpio ji8070a_tf_gpio = {
-	.cd				= {GPIO_PF(20),		LOW_ENABLE},
-	.wp				= {-1,			-1},
-	.pwr				= {-1,			-1},
-};
 
-struct jzmmc_platform_data ji8070a_tf_pdata = {
-	.removal  			= REMOVABLE,
-	.sdio_clk			= 0,
-	.ocr_avail			= MMC_VDD_32_33 | MMC_VDD_33_34,
-	.capacity  			= MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
-#ifdef CONFIG_MMC0_JZ4780
-	.max_freq			= CONFIG_MMC0_MAX_FREQ,
-#endif
-#ifdef CONFIG_MMC2_JZ4780
-	.max_freq			= CONFIG_MMC2_MAX_FREQ,
-#endif
-	.recovery_info			= NULL,
-	.gpio				= &ji8070a_tf_gpio,
-#ifdef CONFIG_MMC0_PIO_MODE
-	.pio_mode			= 1,
-#else
-	.pio_mode			= 0,
-#endif
-	.private_init			= NULL,
-};
-#endif
+#define PXPIN		0x00   /* PIN Level Register */
+#define PXINT		0x10   /* Port Interrupt Register */
+#define PXINTS		0x14   /* Port Interrupt Set Register */
+#define PXINTC		0x18   /* Port Interrupt Clear Register */
+#define PXMSK		0x20   /* Port Interrupt Mask Reg */
+#define PXMSKS		0x24   /* Port Interrupt Mask Set Reg */
+#define PXMSKC		0x28   /* Port Interrupt Mask Clear Reg */
+#define PXPAT1		0x30   /* Port Pattern 1 Set Reg. */
+#define PXPAT1S		0x34   /* Port Pattern 1 Set Reg. */
+#define PXPAT1C		0x38   /* Port Pattern 1 Clear Reg. */
+#define PXPAT0		0x40   /* Port Pattern 0 Register */
+#define PXPAT0S		0x44   /* Port Pattern 0 Set Register */
+#define PXPAT0C		0x48   /* Port Pattern 0 Clear Register */
+#define PXFLG		0x50   /* Port Flag Register */
+#define PXFLGC		0x58   /* Port Flag clear Register */
+#define PXOENS		0x64   /* Port Output Disable Set Register */
+#define PXOENC		0x68   /* Port Output Disable Clear Register */
+#define PXPEN		0x70   /* Port Pull Disable Register */
+#define PXPENS		0x74   /* Port Pull Disable Set Register */
+#define PXPENC		0x78   /* Port Pull Disable Clear Register */
+#define PXDSS		0x84   /* Port Drive Strength set Register */
+#define PXDSC		0x88   /* Port Drive Strength clear Register */
+
+static unsigned int gpio_bakup[4];
+
 int iw8101_wlan_init(void)
 {
 	static struct wake_lock	*wifi_wake_lock = &iw8101_data.wifi_wake_lock;
 	struct regulator *power;
 	int reset;
+
+	gpio_bakup[0] = readl((void *)(0xb0010300 + PXINT)) & 0x1f00000;
+	gpio_bakup[1] = readl((void *)(0xb0010300 + PXMSK)) & 0x1f00000;
+	gpio_bakup[2] = readl((void *)(0xb0010300 + PXPAT1)) & 0x1f00000;
+	gpio_bakup[3] = readl((void *)(0xb0010300 + PXPAT0)) & 0x1f00000;
+
+	writel(0x1f00000, (void *)(0xb0010300 + PXINTC));
+	writel(0x1f00000, (void *)(0xb0010300 + PXMSKS));
+	writel(0x1f00000, (void *)(0xb0010300 + PXPAT1S));
 
 	power = regulator_get(NULL, "vwifi");
 	if (IS_ERR(power)) {
@@ -153,8 +214,18 @@ int IW8101_wlan_power_on(int flag)
 	return -ENODEV;
 start:
 	pr_debug("wlan power on:%d\n", flag);
+
+	writel(gpio_bakup[0] & 0x1f00000, (void *)(0xb0010300 + PXINTS));
+	writel(~gpio_bakup[0] & 0x1f00000, (void *)(0xb0010300 + PXINTC));
+	writel(gpio_bakup[1] & 0x1f00000, (void *)(0xb0010300 + PXMSKS));
+	writel(~gpio_bakup[1] & 0x1f00000, (void *)(0xb0010300 + PXMSKC));
+	writel(gpio_bakup[2] & 0x1f00000, (void *)(0xb0010300 + PXPAT1S));
+	writel(~gpio_bakup[2] & 0x1f00000, (void *)(0xb0010300 + PXPAT1C));
+	writel(gpio_bakup[3] & 0x1f00000, (void *)(0xb0010300 + PXPAT0S));
+	writel(~gpio_bakup[3] & 0x1f00000, (void *)(0xb0010300 + PXPAT0C));
+
 	jzrtc_switch_clk32k(1);
-	mdelay(200);
+	msleep(200);
 
 	switch(flag) {
 		case RESET:
@@ -162,10 +233,10 @@ start:
 			jzmmc_clk_ctrl(1, 1);
 
 			gpio_set_value(reset, 0);
-			mdelay(200);
+			msleep(200);
 
 			gpio_set_value(reset, 1);
-			mdelay(200);
+			msleep(200);
 
 			break;
 
@@ -173,11 +244,11 @@ start:
 			regulator_enable(power);
 
 			gpio_set_value(reset, 0);
-			mdelay(200);
+			msleep(200);
 
 			gpio_set_value(reset, 1);
 
-			mdelay(200);
+			msleep(200);
 			jzmmc_manual_detect(1, 1);
 
 			break;
@@ -224,6 +295,15 @@ start:
 	wake_unlock(wifi_wake_lock);
 
 	jzrtc_switch_clk32k(0);
+
+	gpio_bakup[0] = (unsigned int)readl((void *)(0xb0010300 + PXINT)) & 0x1f00000;
+	gpio_bakup[1] = (unsigned int)readl((void *)(0xb0010300 + PXMSK)) & 0x1f00000;
+	gpio_bakup[2] = (unsigned int)readl((void *)(0xb0010300 + PXPAT1)) & 0x1f00000;
+	gpio_bakup[3] = (unsigned int)readl((void *)(0xb0010300 + PXPAT0)) & 0x1f00000;
+	
+	writel(0x1f00000, (void *)(0xb0010300 + PXINTC));
+	writel(0x1f00000, (void *)(0xb0010300 + PXMSKS));
+	writel(0x1f00000, (void *)(0xb0010300 + PXPAT1S));
 
 	return 0;
 }
