b1d8a06a65ade5b4d45b99314aaae8b9c249bee4
HuZhangYan <zyhu@ingenic.cn>
2012-12-10 16:19:43 +0800

    adjust the backlight settings for q8
    Change-Id: I88a5bb70f9bce2e1768c9da7f335e3c71abbd3cd

--- arch/mips/xburst/soc-4780/board/ji8070a/ji8070a-lcd.c
+++ arch/mips/xburst/soc-4780/board/ji8070a/ji8070a-lcd.c
@@ -348,7 +348,11 @@ static struct platform_pwm_backlight_data ji8070a_backlight_data = {
 	.pwm_id		= 0,
 	.max_brightness	= 255,
 	.dft_brightness	= 80,
+#ifdef CONGIG_Q8
+	.pwm_period_ns	= 1000000,/* 10 KHz */
+#else
 	.pwm_period_ns	= 100000,/* 10 KHz */
+#endif
 	.init		= ji8070a_backlight_init,
 	.exit		= ji8070a_backlight_exit,
     .notify = bk_notify
