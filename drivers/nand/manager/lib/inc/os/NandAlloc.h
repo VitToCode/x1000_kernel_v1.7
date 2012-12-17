24d24b49d6d4c9441e09df247aec398910b5c33b
jbbi <jbbi@ingenic.cn>
2012-12-17 15:16:22 +0800

    use libnm.o instead of NandManager source file
    Change-Id: I3c89fade433c589e52ded20c76a64ed743a3e904

--- drivers/nand/manager/lib/inc/os/NandAlloc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NANDALLOC_H_
-#define _NANDALLOC_H_
-
-void *Nand_VirtualAlloc(int size);
-void Nand_VirtualFree(void *val);
-void *Nand_ContinueAlloc(int size);
-void Nand_ContinueFree(void *val);
-
-#endif /* _NANDALLOC_H_ */
