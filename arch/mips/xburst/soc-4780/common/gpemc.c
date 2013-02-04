0ecd03aaba3bf7921e183cdbf8d0ecf822a4fb3d
SunWenZhong <wzsun@ingenic.cn>
2013-02-04 19:01:38 +0800

    soc4780: initial codes for gpemc(nemc)
    Change-Id: I336e377fd62c0bcff642a783c7ca8738970c0f15

--- arch/mips/xburst/soc-4780/common/gpemc.c
+++ arch/mips/xburst/soc-4780/common/gpemc.c
@@ -21,6 +21,54 @@
 #include <soc/base.h>
 #include <soc/gpemc.h>
 
+#define GPEMC_REGS_FILE_BASE 	0x13410000
+#define CPEMC_NAND_REGS_FILE_BASE	0x13410050
+
+#define GPNEMC_CS1_IOBASE 0x1b000000
+#define GPNEMC_CS2_IOBASE 0x1a000000
+#define GPNEMC_CS3_IOBASE 0x19000000
+#define GPNEMC_CS4_IOBASE 0x18000000
+#define GPNEMC_CS5_IOBASE 0x17000000
+#define GPNEMC_CS6_IOBASE 0x16000000
+
+static struct {
+	volatile u32 nfcsr;
+
+	volatile u32 pad0[(0x13410100 - CPEMC_NAND_REGS_FILE_BASE)
+	                  / sizeof(u32)];
+
+	volatile u32 pncr;
+	volatile u32 pndr;
+	volatile u32 bitcnt;
+	volatile u32 tgwe;
+	volatile u32 tgcr1;
+	volatile u32 tgcr2;
+	volatile u32 tgcr3;
+	volatile u32 tgcr4;
+	volatile u32 tgcr5;
+	volatile u32 tgcr6;
+	volatile u32 tgsr;
+	volatile u32 tgfl;
+	volatile u32 tgfh;
+	volatile u32 tgcl;
+	volatile u32 tgch;
+	volatile u32 tgpd;
+	volatile u32 tgsl;
+	volatile u32 tgsh;
+	volatile u32 tgrr;
+	volatile u32 tgdr;
+	volatile u32 tghl;
+	volatile u32 tghh;
+}__iomem * const gpemc_nand_regs_file =
+		(typeof(gpemc_nand_regs_file))
+			CKSEG1ADDR(CPEMC_NAND_REGS_FILE_BASE);
+
+static struct resource gpemc_nand_regs_mem = {
+	.start = CPEMC_NAND_REGS_FILE_BASE,
+	.end = CPEMC_NAND_REGS_FILE_BASE +
+			sizeof(*gpemc_nand_regs_file) - 1,
+};
+
 static struct {
 	volatile u32 pad0[0x14 / sizeof(u32)];
 
@@ -39,45 +87,46 @@ static struct {
 	volatile u32 SACR4;
 	volatile u32 SACR5;
 	volatile u32 SACR6;
-}__iomem * const gpemc_regs_file = (typeof(gpemc_regs_file)) CKSEG1ADDR(
-		NEMC_IOBASE);
+}__iomem * const gpemc_regs_file =
+		(typeof(gpemc_regs_file))
+			CKSEG1ADDR(GPEMC_REGS_FILE_BASE);
 
 static struct resource gpemc_regs_mem = {
-	.start = NEMC_IOBASE,
-	.end = NEMC_IOBASE + sizeof(*gpemc_regs_file) - 1,
+	.start = GPEMC_REGS_FILE_BASE,
+	.end = GPEMC_REGS_FILE_BASE + sizeof(*gpemc_regs_file) - 1,
 };
 
-#define GPEMC_BANK_SIZE (NEMC_CS1_IOBASE - NEMC_CS2_IOBASE)
+#define GPEMC_BANK_SIZE (GPNEMC_CS1_IOBASE - GPNEMC_CS2_IOBASE)
 static struct resource gpemc_banks_mem[7] = {
 	{},
 
 	{
-		.start = NEMC_CS1_IOBASE,
+		.start = GPNEMC_CS1_IOBASE,
 		.end = GPEMC_BANK_SIZE - 1,
 	},
 
 	{
-		.start = NEMC_CS2_IOBASE,
+		.start = GPNEMC_CS2_IOBASE,
 		.end = GPEMC_BANK_SIZE - 1,
 	},
 
 	{
-		.start = NEMC_CS3_IOBASE,
+		.start = GPNEMC_CS3_IOBASE,
 		.end = GPEMC_BANK_SIZE - 1,
 	},
 
 	{
-		.start = NEMC_CS4_IOBASE,
+		.start = GPNEMC_CS4_IOBASE,
 		.end = GPEMC_BANK_SIZE - 1,
 	},
 
 	{
-		.start = NEMC_CS5_IOBASE,
+		.start = GPNEMC_CS5_IOBASE,
 		.end = GPEMC_BANK_SIZE - 1,
 	},
 
 	{
-		.start = NEMC_CS6_IOBASE,
+		.start = GPNEMC_CS6_IOBASE,
 		.end = GPEMC_BANK_SIZE - 1,
 	}
 };
@@ -101,7 +150,14 @@ int __init gpemc_init(void) {
 	res = request_mem_region(gpemc_regs_mem.start,
 			resource_size(&gpemc_regs_mem), "gpemc-regs-mem");
 	if (!res) {
-		pr_err("gpemc: grab regs file failed, err: %d\n", -EBUSY);
+		pr_err("gpemc: grab gpemc regs file failed\n");
+		goto err_return;
+	}
+
+	res = request_mem_region(gpemc_nand_regs_mem.start,
+			resource_size(&gpemc_nand_regs_mem), "gpemc-nand-regs-mem");
+	if (!res) {
+		pr_err("gpemc: grab gpemc nand regs file failed\n");
 		goto err_return;
 	}
 
@@ -111,6 +167,7 @@ int __init gpemc_init(void) {
 	return 0;
 
 err_return:
+	BUG();
 	return -EBUSY;
 }
 
@@ -163,8 +220,10 @@ EXPORT_SYMBOL(gpemc_request_cs);
 void gpemc_release_cs(struct gpemc_bank* bank)
 {
 	BUG_ON(bank->cs < 1 || bank->cs >= ARRAY_SIZE(gpemc_banks_mem));
-	if (!test_bit(bank->cs, bank_use_map))
+	if (!test_bit(bank->cs, bank_use_map)) {
+		WARN(1, "try to release a free cs.\n");
 		return;
+	}
 
 	release_mem_region(gpemc_banks_mem[bank->cs].start,
 			resource_size(&gpemc_banks_mem[bank->cs]));
