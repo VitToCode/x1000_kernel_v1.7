56c221fa63f1a9b452abb44ff83c4278605afc9d
cli <cli@ingenic.cn>
2013-03-02 17:12:55 +0800

    1,reduce audio pop about audio driver of jz4780 . 2,Add codec driver about audio driver of jz4775
    Change-Id: I4c8b51f6cf467784647dda32335dff5c47b23aec

--- arch/mips/xburst/soc-4775/board/mensa/board.h
+++ arch/mips/xburst/soc-4775/board/mensa/board.h
@@ -34,6 +34,25 @@ extern struct jzfb_platform_data jzfb1_pdata;
 #define GPIO_TP_WAKE		GPIO_PB(28)
 #define GPIO_TP_INT		GPIO_PB(29)
 
+#define GPIO_HP_MUTE		-1	/*hp mute gpio*/
+#define GPIO_HP_MUTE_LEVEL		-1		/*vaild level*/
+
+#define GPIO_SPEAKER_EN			-1/*speaker enable gpio*/
+#define GPIO_SPEAKER_EN_LEVEL	-1
+
+#define GPIO_HANDSET_EN		  -1		/*handset enable gpio*/
+#define GPIO_HANDSET_EN_LEVEL -1
+
+#define	GPIO_HP_DETECT		-1	/*hp detect gpio*/
+#define GPIO_HP_INSERT_LEVEL	-1
+#define GPIO_MIC_SELECT		-1		/*mic select gpio*/
+#define GPIO_BUILDIN_MIC_LEVEL	-1		/*builin mic select level*/
+#define GPIO_MIC_DETECT		-1
+#define GPIO_MIC_INSERT_LEVEL -1
+#define GPIO_MIC_DETECT_EN		-1  /*mic detect enable gpio*/
+#define GPIO_MIC_DETECT_EN_LEVEL	-1 /*mic detect enable gpio*/
+
+
 /**
  * KEY gpio
  **/
@@ -51,12 +70,6 @@ extern struct jzfb_platform_data jzfb1_pdata;
  * USB detect pin
  **/
 #define GPIO_USB_DETE                   GPIO_PA(16)
-
-/**
- *audio gpio
- **/
-#define GPIO_SPEAKER_SHUTDOWN	GPIO_PG(14)
-#define GPIO_HP_DETECT		GPIO_PA(17)
 
 /**
  * sound platform data
