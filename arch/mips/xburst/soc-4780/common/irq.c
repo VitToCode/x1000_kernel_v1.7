99d7457a99f52884240ff67ebbdb3cc32e14ac21
hyang <hyang@ingenic.cn>
2013-03-12 11:57:28 +0800

    add a irq_gap_time_monitor proc
    Change-Id: Ic83d8bc210491bc3d774813e4186f316e28d535a

--- arch/mips/xburst/soc-4780/common/irq.c
+++ arch/mips/xburst/soc-4780/common/irq.c
@@ -15,13 +15,17 @@
 #include <linux/interrupt.h>
 #include <linux/bitops.h>
 
+#include <linux/proc_fs.h>
+
 #include <asm/irq_cpu.h>
 
 #include <soc/base.h>
 #include <soc/irq.h>
 #include <soc/ost.h>
 
+#include <linux/io.h>
 #include <smp_cp0.h>
+#include <linux/uaccess.h>
 
 #define TRACE_IRQ        1
 #define PART_OFF	0x20
@@ -38,6 +42,114 @@
 static void __iomem *intc_base;
 static unsigned long intc_saved[2];
 static unsigned long intc_wakeup[2];
+//#define IRQ_TIME_MONITOR_DEBUG
+#ifdef IRQ_TIME_MONITOR_DEBUG
+unsigned long long time_monitor[500] = {0};
+int time_over[500] = {0};
+int echo_success = 0;
+#endif
+
+#ifdef IRQ_TIME_MONITOR_DEBUG
+enum {
+	IRQ_ID_AIC = 9,
+#define IRQ_NAME_AIC		"aic_irq"
+	IRQ_ID_HDMI = 11,
+#define IRQ_NAME_HDMI		"HDMI TX_INT"
+	IRQ_ID_PDMA = 18,
+#define IRQ_NAME_PDMA		"pdma"
+	IRQ_ID_GPIO_F = 20,
+#define IRQ_NAME_GPIO_F		"GPIO F"
+	IRQ_ID_GPIO_E = 21,
+#define IRQ_NAME_GPIO_E		"GPIO E"
+	IRQ_ID_GPIO_D = 22,
+#define IRQ_NAME_GPIO_D		"GPIO D"
+	IRQ_ID_GPIO_C = 23,
+#define IRQ_NAME_GPIO_C		"GPIO C"
+	IRQ_ID_GPIO_B = 24,
+#define IRQ_NAME_GPIO_B		"GPIO B"
+	IRQ_ID_GPIO_A = 25,
+#define IRQ_NAME_GPIO_A		"GPIO A"
+
+	/**********************************************************************************/
+	IRQ_ID_X2D = 27,
+#define IRQ_NAME_X2D		"x2d"
+	IRQ_ID_OTG = 29,
+#define IRQ_NAME_OTG		"dwc_otg, dwc_otg_pcd, dwc_otg_hcd:usb1"
+	IRQ_ID_IPU0 = 30,
+#define IRQ_NAME_IPU0		"ipu0"
+	IRQ_ID_LCDC1 = 31,
+#define IRQ_NAME_LCDC1		"lcdc1"
+	IRQ_ID_CPU0TIMER = 34,
+#define IRQ_NAME_CPU0TIMER	"CPU0 jz-timerirq"
+	IRQ_ID_CPU1TIMER = 35,
+#define IRQ_NAME_CPU1TIMER	"CPU1 jz-timerirq"
+	IRQ_ID_IPU1 = 37,
+#define IRQ_NAME_IPU1		"ipu1"
+	IRQ_ID_CIM = 38,
+#define IRQ_NAME_CIM		"jz-cim"
+	IRQ_ID_LCDC0 = 39,
+#define IRQ_NAME_LCDC0		"lcdc0"
+	IRQ_ID_RTC = 40,
+#define IRQ_NAME_RTC		"rtc 1Hz and alarm"
+	IRQ_ID_MMC1 = 44,
+#define IRQ_NAME_MMC1		"jzmmc.1"
+	IRQ_ID_MMC0 = 45,
+#define IRQ_NAME_MMC0		"jzmmc.0"
+	IRQ_ID_AOSD = 51,
+#define IRQ_NAME_AOSD		"jz-aosd"
+	IRQ_ID_UART3 = 56,
+#define IRQ_NAME_UART3		"uart3"
+	IRQ_ID_I2C3 = 65,
+#define IRQ_NAME_I2C3		"jz-i2c.3"
+	IRQ_ID_I2C1 = 67,
+#define IRQ_NAME_I2C1		"jz-i2c.1"
+	IRQ_ID_PDMAM = 69,
+#define IRQ_NAME_PDMAM		"pdmam"
+	IRQ_ID_VPU = 70,
+#define IRQ_NAME_VPU		"vpu"
+	IRQ_ID_SGXISR = 71,
+#define IRQ_NAME_SGXISR		"SGX ISR"
+};
+
+struct irq_timer_monitor
+{
+	const char *name;
+};
+
+static struct irq_timer_monitor irq_srcs[] = {
+#define DEF_IRQ(N)						\
+		[IRQ_ID_##N] = { .name = IRQ_NAME_##N }
+
+		DEF_IRQ(AIC),
+		DEF_IRQ(HDMI),
+		DEF_IRQ(PDMA),
+		DEF_IRQ(GPIO_F),
+		DEF_IRQ(GPIO_E),
+		DEF_IRQ(GPIO_D),
+		DEF_IRQ(GPIO_C),
+		DEF_IRQ(GPIO_B),
+		DEF_IRQ(GPIO_A),
+		DEF_IRQ(OTG),
+		DEF_IRQ(IPU0),
+		DEF_IRQ(LCDC1),
+		DEF_IRQ(CPU0TIMER),
+		DEF_IRQ(CPU1TIMER),
+		DEF_IRQ(IPU1),
+		DEF_IRQ(CIM),
+		DEF_IRQ(LCDC0),
+		DEF_IRQ(RTC),
+		DEF_IRQ(MMC1),
+		DEF_IRQ(MMC0),
+		DEF_IRQ(AOSD),
+		DEF_IRQ(UART3),
+		DEF_IRQ(I2C3),
+		DEF_IRQ(I2C1),
+		DEF_IRQ(PDMAM),
+		DEF_IRQ(VPU),
+		DEF_IRQ(SGXISR),
+#undef DEF_IRQ
+};
+#endif
 
 extern void __enable_irq(struct irq_desc *desc, unsigned int irq, bool resume);
 
@@ -205,6 +317,11 @@ void __init arch_init_irq(void)
 
 static void intc_irq_dispatch(void)
 {
+#ifdef IRQ_TIME_MONITOR_DEBUG
+	unsigned long long time_num[2];
+	unsigned long long time_sub;
+	unsigned int	irq_con;
+#endif
 	unsigned long ipr[2],gpr[2];
 	unsigned long ipr_intc;
 #ifdef CONFIG_SMP
@@ -225,6 +342,39 @@ static void intc_irq_dispatch(void)
 	gpr[1] = ipr[1] & 0x9f0f0004;
 	ipr[1] &= ~0x9f0f0004;
 
+#ifdef IRQ_TIME_MONITOR_DEBUG
+	time_num[0] = sched_clock();
+	if(gpr[1] & 0x1f000000) {
+		generic_handle_irq(ffs(gpr[1]) +31 +IRQ_INTC_BASE);
+		irq_con = ffs(gpr[1]) +31 +IRQ_INTC_BASE;
+	}else {
+
+		if (ipr[0]) {
+			generic_handle_irq(ffs(ipr[0]) -1 +IRQ_INTC_BASE);
+			irq_con = ffs(ipr[0]) -1 +IRQ_INTC_BASE;
+		}
+		if (gpr[0]) {
+			generic_handle_irq(ffs(gpr[0]) -1 +IRQ_INTC_BASE);
+			irq_con = ffs(gpr[0]) -1 +IRQ_INTC_BASE;
+		}
+
+		if (ipr[1]) {
+			generic_handle_irq(ffs(ipr[1]) +31 +IRQ_INTC_BASE);
+			irq_con = ffs(ipr[1]) +31 +IRQ_INTC_BASE;
+		}
+		if (gpr[1]) {
+			generic_handle_irq(ffs(gpr[1]) +31 +IRQ_INTC_BASE);
+			irq_con = ffs(gpr[1]) +31 +IRQ_INTC_BASE;
+		}
+	}
+	time_num[1] = sched_clock();
+	time_sub = time_num[1] - time_num[0];
+	if (echo_success && (time_sub > time_monitor[499]))
+		time_over[irq_con] += 1;
+	if (time_sub > time_monitor[irq_con])
+		time_monitor[irq_con] = time_sub;
+#else
+
 	if (ipr[0]) {
 		do_IRQ(ffs(ipr[0]) -1 +IRQ_INTC_BASE);
 	}
@@ -238,6 +388,8 @@ static void intc_irq_dispatch(void)
 	if (gpr[1]) {
 		generic_handle_irq(ffs(gpr[1]) +31 +IRQ_INTC_BASE);
 	}
+#endif
+
 
 #ifdef CONFIG_SMP
 	nextcpu = switch_cpu_irq(cpuid);
@@ -305,3 +457,76 @@ void arch_suspend_enable_irqs(void)
 	writel(0xffffffff & ~intc_saved[1], intc_base + PART_OFF + IMCR_OFF);
 	local_irq_enable();
 }
+
+#ifdef IRQ_TIME_MONITOR_DEBUG
+/* cat gap_time_irq to show it */
+static int irq_read_proc(char *page, char **start, off_t off,
+		int count, int *eof, void *data)
+{
+	int len = 0;
+	int i;
+	struct irq_desc *desc;
+
+#define PRINT(ARGS...) len += sprintf (page+len, ##ARGS)
+	PRINT("ID NAME       gap_time   over_times       dev_name\n");
+	for(i = 0; i < 499; i++) {
+		if (time_monitor[i] == 0)
+			continue;
+		else {
+			desc = irq_to_desc(i);
+			if (desc->action == NULL)
+				PRINT("%3d %18lld   %10d       %s \n", i, time_monitor[i], time_over[i], irq_srcs[i].name);
+			else
+				PRINT("%3d %18lld   %10d       %s \n", i, time_monitor[i], time_over[i],desc->action->name);
+		}
+	}
+	PRINT("EER: \n");
+
+	return len;
+}
+
+/* echo value to clear time_monitor value*/
+static int irq_write_proc(struct file *file, const char __user *buffer,
+		unsigned long count, void *data)
+{
+	int ret, i;
+	char buf[32];
+	unsigned long long cgr;
+
+	if (count > 32)
+		count = 32;
+	if (copy_from_user(buf, buffer, count))
+		return -EFAULT;
+
+	ret = sscanf(buf,"%lld", &cgr);
+	if (cgr == 0)
+		for (i = 0; i < 499; i++) {
+			time_monitor[i] = 0;
+			time_over[i] = 0;
+			if (i == 498)
+				printk(" clear time_monitor successed!! \n");
+		}
+	else {
+		time_monitor[499] = cgr;
+		printk(" echo value successed: %lld \n", time_monitor[499]);
+		echo_success = 1;
+	}
+
+	return count;
+}
+
+static int __init init_irq_proc_time(void)
+{
+	struct proc_dir_entry *res;
+
+	res = create_proc_entry("gap_time_irq", 0666, NULL);
+	if (res) {
+		res->read_proc = irq_read_proc;
+		res->write_proc = irq_write_proc;
+		res->data = NULL;
+	}
+	return 0;
+}
+
+module_init(init_irq_proc_time);
+#endif
