3d26d3e5993816f0dc3ae07c032f7d4350c7b0c4
SunWenZhong <wzsun@ingenic.cn>
2013-04-10 13:30:55 +0800

    fb: add CONFIG_FB_VSYNC_SKIP
    Change-Id: Ic6eb133c87f97a4899003cd49afd40acf6dfd712

--- drivers/video/jz4780-fb/jz4780_fb.c
+++ drivers/video/jz4780-fb/jz4780_fb.c
@@ -2599,38 +2599,49 @@ static ssize_t vsync_skip_r(struct device *dev, struct device_attribute *attr,
 	return 3;	/* sizeof ("%d\n") */
 }
 
-static ssize_t vsync_skip_w(struct device *dev, struct device_attribute *attr,
-			    const char *buf, size_t count)
-{
-	struct jzfb *jzfb = dev_get_drvdata(dev);
+static int vsync_skip_set(struct jzfb *jzfb, int vsync_skip) {
 	unsigned int map_wide10 = 0;
 	int rate, i, p, n;
 	int fake_float_1k;
 
-	if ((count != 1) && (count != 2))
-		return -EINVAL;
-	if ((*buf < '0') && (*buf > '9'))
+	if (vsync_skip < 0 || vsync_skip > 9)
 		return -EINVAL;
 
-	rate = *buf - '0' + 1;			/* 1 ~ 10 */
-        fake_float_1k = 10000 / rate;		/* 10.0 / rate */
+	rate = vsync_skip + 1;
+	fake_float_1k = 10000 / rate; /* 10.0 / rate */
 
 	p = 1;
-	n = (fake_float_1k * p + 500) / 1000;	/* +0.5 to int */
+	n = (fake_float_1k * p + 500) / 1000; /* +0.5 to int */
 
-        for (i = 1; i <= 10; i++) {
+	for (i = 1; i <= 10; i++) {
 		map_wide10 = map_wide10 << 1;
 		if (i == n) {
-		        map_wide10++;
+			map_wide10++;
 			p++;
 			n = (fake_float_1k * p + 500) / 1000;
 		}
 	}
 	mutex_lock(&jzfb->lock);
 	jzfb->vsync_skip_map = map_wide10;
-	jzfb->vsync_skip_ratio = rate - 1;	/* 0 ~ 9 */
-        mutex_unlock(&jzfb->lock);
-        return count;
+	jzfb->vsync_skip_ratio = rate - 1; /* 0 ~ 9 */
+	mutex_unlock(&jzfb->lock);
+
+	return 0;
+}
+
+static ssize_t vsync_skip_w(struct device *dev, struct device_attribute *attr,
+			    const char *buf, size_t count)
+{
+	struct jzfb *jzfb = dev_get_drvdata(dev);
+
+	if ((count != 1) && (count != 2))
+		return -EINVAL;
+	if ((*buf < '0') && (*buf > '9'))
+		return -EINVAL;
+
+	vsync_skip_set(jzfb, *buf - '0');
+
+	return count;
 }
 
 static struct device_attribute lcd_sysfs_attrs[] = {
@@ -2786,8 +2797,8 @@ static int __devinit jzfb_probe(struct platform_device *pdev)
 		goto err_free_irq;
 	}
 
-	jzfb->vsync_skip_ratio = 9;
-	jzfb->vsync_skip_map = 0x3ff;
+	vsync_skip_set(jzfb, CONFIG_FB_VSYNC_SKIP);
+
 	init_waitqueue_head(&jzfb->vsync_wq);
 	jzfb->vsync_thread = kthread_run(jzfb_wait_for_vsync_thread,
 					 jzfb, "jzfb-vsync");
