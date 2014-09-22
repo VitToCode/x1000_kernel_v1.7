208d8484ef5a6122f5b233313e94b61dd660de2a
pzqi <pzqi_bj@ingenic.cn>
2014-09-22 17:39:44 +0800

    1.fix bt audio replay error state.

--- arch/mips/xburst/soc-m200/board/dorado/dorado_v30/board.h
+++ arch/mips/xburst/soc-m200/board/dorado/dorado_v30/board.h
@@ -179,6 +179,9 @@
 #define GPIO_MIC_INSERT_LEVEL   -1
 #define GPIO_MIC_DETECT_EN	-1  /*mic detect enable gpio*/
 #define GPIO_MIC_DETECT_EN_LEVEL -1 /*mic detect enable gpio*/
+
+#define HP_SENSE_ACTIVE_LEVEL	0
+#define HOOK_ACTIVE_LEVEL		-1
 /* ****************************GPIO AUDIO END******************************** */
 
 /* ****************************GPIO WIFI START******************************* */
