8f42ab45c25e88e4dab22b062dbee1192bf9fdc4
YanZhengting <ztyan@ingenic.cn>
2014-12-25 12:17:55 +0800

    add new libdmmu support for ipu and vpu

---
+++ arch/mips/xburst/soc-m200/include/mach/libdmmu.h
@@ -0,0 +1,8 @@
+#ifndef _LIBDMMU_
+#define _LIBDMMU_
+
+unsigned long dmmu_map(unsigned long vaddr,unsigned long len);
+int dmmu_unmap(unsigned long vaddr, int len);
+int dmmu_unmap_all(void);
+
+#endif
