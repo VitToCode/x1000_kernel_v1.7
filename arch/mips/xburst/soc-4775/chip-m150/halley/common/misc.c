fd465786059e8090868db4e3a848157c6e97472c
sccheng <sccheng_bj@ingenic.cn>
2015-02-28 13:51:32 +0800

    add leds-gpio driver

--- arch/mips/xburst/soc-4775/chip-m150/halley/common/misc.c
+++ arch/mips/xburst/soc-4775/chip-m150/halley/common/misc.c
@@ -4,6 +4,7 @@
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
 #include <linux/tsc.h>
+#include <linux/leds.h>
 #include <linux/jz4780-adc.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_gpio.h>
@@ -16,7 +17,6 @@
 #include <gpio.h>
 #include <linux/jz_dwc.h>
 #include <linux/interrupt.h>
-#include <sound/jz-aic.h>
 #include "board_base.h"
 #include <board.h>
 
@@ -24,5 +24,40 @@
 struct jz_efuse_platform_data jz_efuse_pdata = {
 	    /* supply 2.5V to VDDQ */
 	    .gpio_vddq_en_n = GPIO_EFUSE_VDDQ,
+};
+#endif
+
+#ifdef CONFIG_LEDS_GPIO
+struct gpio_led jz_leds[] = {
+	[0]={
+		.name = "wl_led_r",
+		.gpio = WL_LED_R,
+		.active_low = 0,
+	},
+	[1]={
+		.name = "wl_led_g",
+		.gpio = WL_LED_G,
+		.active_low = 0,
+	},
+
+	[2]={
+		.name = "wl_led_b",
+		.gpio = WL_LED_B,
+		.active_low = 0,
+	},
+
+};
+
+struct gpio_led_platform_data  jz_led_pdata = {
+	.num_leds = 3,
+	.leds = jz_leds,
+};
+
+struct platform_device jz_led_rgb = {
+	.name       = "leds-gpio",
+	.id     = -1,
+	.dev        = {
+		.platform_data  = &jz_led_pdata,
+	}
 };
 #endif
