cebe15b328b29c4cf8ef69b8b7be9d7c016a11dd
HuXiaohui <xhhu_bj@ingenic.cn>
2015-08-04 15:51:18 +0800

    modify GPIO_SPEAKER_EN for canna

--- arch/mips/xburst/soc-x1000/chip-x1000/canna/canna_v10/board.h
+++ arch/mips/xburst/soc-x1000/chip-x1000/canna/canna_v10/board.h
@@ -46,7 +46,7 @@
 #define GPIO_HP_MUTE        -1  /*hp mute gpio*/
 #define GPIO_HP_MUTE_LEVEL  -1  /*vaild level*/
 
-#define GPIO_SPEAKER_EN    GPIO_PB(0)         /*speaker enable gpio*/
+#define GPIO_SPEAKER_EN    GPIO_PC(25)         /*speaker enable gpio*/
 #define GPIO_SPEAKER_EN_LEVEL   0
 
 #define GPIO_HANDSET_EN     -1  /*handset enable gpio*/
