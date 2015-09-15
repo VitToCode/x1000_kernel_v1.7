ad037fa89caef4f915146567567c8c00b1e8f307
SunJingxin <jxsun@ingenic.cn>
2015-09-15 09:48:54 +0800

    add doss board config
    Change-Id: I1ff0c87f0d897c7c2a2c9ed2fd37ed87958a8a9f

---
+++ arch/mips/xburst/soc-x1000/chip-x1000/doss/common/board_base.h
@@ -0,0 +1,84 @@
+#ifndef __BOARD_BASE_H__
+#define __BOARD_BASE_H__
+
+#include <board.h>
+#include <linux/spi/spi.h>
+#include <linux/i2c.h>
+
+extern struct snd_codec_data codec_data;
+
+#ifdef CONFIG_JZ_INTERNAL_CODEC_V12
+extern struct snd_codec_data codec_data;
+#endif
+
+#ifdef CONFIG_JZMMC_V12_MMC0
+extern struct jzmmc_platform_data tf_pdata;
+#endif
+#ifdef CONFIG_JZMMC_V12_MMC1
+extern struct jzmmc_platform_data sdio_pdata;
+#endif
+
+#ifdef CONFIG_JZMMC_V11_MMC2
+extern struct jzmmc_platform_data sdio_pdata;
+#endif
+
+#ifdef CONFIG_BROADCOM_RFKILL
+extern struct platform_device	bt_power_device;
+extern struct platform_device	bluesleep_device;
+#endif
+
+#ifdef CONFIG_BCM_AP6212_RFKILL
+extern struct platform_device   bt_power_device;
+#endif
+
+#ifdef CONFIG_USB_DWC2
+extern struct platform_device   jz_dwc_otg_device;
+#endif
+
+#ifdef CONFIG_JZ_SPI0
+extern struct jz_spi_info spi0_info_cfg;
+#endif
+#ifdef CONFIG_JZ_SFC
+extern struct jz_sfc_info sfc_info_cfg;
+#endif
+#ifdef CONFIG_JZ_SPI_NOR
+extern struct spi_board_info jz_spi0_board_info[];
+extern int jz_spi0_devs_size;
+#endif
+#ifdef CONFIG_MTD_JZ_SPI_NORFLASH
+extern struct spi_board_info jz_spi0_board_info[];
+extern int jz_spi0_devs_size;
+#endif
+#ifdef CONFIG_SPI_GPIO
+extern struct platform_device jz_spi_gpio_device;
+#endif
+
+#ifdef CONFIG_JZ_EFUSE_V11
+extern struct jz_efuse_platform_data jz_efuse_pdata;
+#endif
+
+#ifdef CONFIG_KEYBOARD_MATRIX
+extern struct platform_device jz_matrix_kdb_device;
+#endif
+
+#ifdef CONFIG_LEDS_GPIO
+extern struct platform_device jz_leds_gpio;
+#endif
+
+#ifdef CONFIG_KEYBOARD_JZGPIO
+extern struct platform_device jz_longbutton_device;
+#endif
+
+/* JZ LCD driver */
+#ifdef CONFIG_BACKLIGHT_PWM
+extern struct platform_device backlight_device;
+#endif
+#ifdef CONFIG_FB_JZ_V13
+extern struct jzfb_platform_data jzfb_pdata;
+#endif
+#ifdef CONFIG_LCD_XRM2002903
+extern struct platform_device xrm2002903_device;
+#endif
+/* end of LCD driver */
+
+#endif
