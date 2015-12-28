85327a5028498129324b3d560193707d4cca9264
JinTao <tjin@ingenic.cn>
2015-12-28 10:22:13 +0800

    audio OSS: resolve a problem that the delay time between i2s and dmic enable record, is not fixed.
    Change-Id: I402fa3d90e9be07b1ef77f7168ac7bf33ecc22b5

--- sound/oss/jzsound/interface/xb_snd_dsp.c
+++ sound/oss/jzsound/interface/xb_snd_dsp.c
@@ -2706,16 +2706,18 @@ long xb_snd_dsp_ioctl(struct file *file,
                         dp = endpoints->out_endpoint;
 
                 spin_lock_irqsave(&dp->pipe_lock, lock_flags);
-
-                if (dmic_ddata->dev_ioctl) {
-                        dmic_ddata->dev_ioctl(SND_DSP_ENABLE_DMA_RX, 0);
-                } else if(dmic_ddata->dev_ioctl_2) {
-                        dmic_ddata->dev_ioctl_2(dmic_ddata, SND_DSP_ENABLE_DMA_RX, 0);
-                }
+		/* The order of i2s and dmic enable dma rx, should not reverse,
+		 * because of the flush_work_sync function in i2s dev_ioctl.
+		 */
                 if (i2s_ddata->dev_ioctl) {
                         i2s_ddata->dev_ioctl(SND_DSP_ENABLE_DMA_RX, 0);
                 } else if(i2s_ddata->dev_ioctl_2) {
                         i2s_ddata->dev_ioctl_2(i2s_ddata, SND_DSP_ENABLE_DMA_RX, 0);
+                }
+                if (dmic_ddata->dev_ioctl) {
+                        dmic_ddata->dev_ioctl(SND_DSP_ENABLE_DMA_RX, 0);
+                } else if(dmic_ddata->dev_ioctl_2) {
+                        dmic_ddata->dev_ioctl_2(dmic_ddata, SND_DSP_ENABLE_DMA_RX, 0);
                 }
                 spin_unlock_irqrestore(&dp->pipe_lock, lock_flags);
                 ret = 0;
