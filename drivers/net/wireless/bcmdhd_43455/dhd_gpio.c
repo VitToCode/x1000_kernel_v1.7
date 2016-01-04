281bbf3fc017f63a34fa2d8a535ba5ad6994cca1
zhengwei.zhang <zhengwei.zhang@ingenic.com>
2016-01-04 10:22:18 +0800

    Update ap6255 wifi driver

--- drivers/net/wireless/bcmdhd_43455/dhd_gpio.c
+++ drivers/net/wireless/bcmdhd_43455/dhd_gpio.c
@@ -1,7 +1,6 @@
 
 #include <osl.h>
-#include <dngl_stats.h>
-#include <dhd.h>
+#include <dhd_linux.h>
 #include <mach/jzmmc.h>
 
 #ifdef CONFIG_MACH_ODROID_4210
@@ -38,7 +37,7 @@ uint bcm_wlan_get_oob_irq(void)
 #endif
 	host_oob_irq = get_host_wake_oob();
 
-	printf("host_oob_irq: %d \r\n", host_oob_irq);
+	printf("host_oob_irq: %d\n", host_oob_irq);
 
 	return host_oob_irq;
 }
@@ -49,12 +48,17 @@ uint bcm_wlan_get_oob_irq_flags(void)
 
 //#ifdef CONFIG_MACH_ODROID_4210
 #ifdef HW_OOB
+#ifdef HW_OOB_LOW_LEVEL
+	host_oob_irq_flags = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWLEVEL | IORESOURCE_IRQ_SHAREABLE;
+#else
 	host_oob_irq_flags = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE;
+#endif
 #else
 	host_oob_irq_flags = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE | IORESOURCE_IRQ_SHAREABLE;
 #endif
 //#endif
-	printf("host_oob_irq_flags=%d\n", host_oob_irq_flags);
+
+	printf("host_oob_irq_flags=0x%X\n", host_oob_irq_flags);
 
 	return host_oob_irq_flags;
 }
@@ -64,6 +68,7 @@ int bcm_wlan_set_power(bool on)
 {
 #if 0
 	int err = 0;
+
 	if (on) {
 		printf("======== PULL WL_REG_ON HIGH! ========\n");
 #ifdef CONFIG_MACH_ODROID_4210
@@ -161,6 +166,38 @@ void* bcm_wlan_prealloc(int section, unsigned long size)
 }
 #endif
 
+#if !defined(WL_WIRELESS_EXT)
+struct cntry_locales_custom {
+	char iso_abbrev[WLC_CNTRY_BUF_SZ];	/* ISO 3166-1 country abbreviation */
+	char custom_locale[WLC_CNTRY_BUF_SZ];	/* Custom firmware locale */
+	int32 custom_locale_rev;		/* Custom local revisin default -1 */
+};
+#endif
+
+static struct cntry_locales_custom brcm_wlan_translate_custom_table[] = {
+	/* Table should be filled out based on custom platform regulatory requirement */
+	{"",   "XT", 49},  /* Universal if Country code is unknown or empty */
+	{"US", "US", 0},
+};
+
+static void *bcm_wlan_get_country_code(char *ccode)
+{
+	struct cntry_locales_custom *locales;
+	int size;
+	int i;
+
+	if (!ccode)
+		return NULL;
+
+	locales = brcm_wlan_translate_custom_table;
+	size = ARRAY_SIZE(brcm_wlan_translate_custom_table);
+
+	for (i = 0; i < size; i++)
+		if (strcmp(ccode, locales[i].iso_abbrev) == 0)
+			return &locales[i];
+	return NULL;
+}
+
 int bcm_wlan_set_plat_data(void) {
 	printf("======== %s ========\n", __FUNCTION__);
 	dhd_wlan_control.set_power = bcm_wlan_set_power;
@@ -169,6 +206,7 @@ int bcm_wlan_set_plat_data(void) {
 #ifdef CONFIG_DHD_USE_STATIC_BUF
 	dhd_wlan_control.mem_prealloc = bcm_wlan_prealloc;
 #endif
+	dhd_wlan_control.get_country_code = bcm_wlan_get_country_code;
 	return 0;
 }
 
